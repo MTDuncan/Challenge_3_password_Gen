cf056a3203f6449fa241ff9c4e124de0d3929e44
Matthew <mduncan766@gmail.com>
2022-12-22 01:49:19 -0600

    Cleaning format of page

--- friendly-parakeet-main/friendly-parakeet-main/Develop/script.js
+++ friendly-parakeet-main/friendly-parakeet-main/Develop/script.js
@@ -7,11 +7,18 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
 let length = prompt("How many characters would you like your Password to be?")
+
+
 let numbers = prompt("Would you like the password to contain numbers?")
+
 let letters = prompt("Would you like your password to contain letters?")
+
 let upperCase = prompt("Would you like lower-case in your password?")
+
 let upperCase = prompt("would you like upper-case in your password?")
+
 let specialCharacters = prompt("Would you like special characters in your password?")
+
 function generatePassword(){
 
 
